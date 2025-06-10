78ab1372372e2dfeefb401e7c58f088bfa2237e7
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-06-10 22:56:16 +0700

    Update CreateAccountActivity.java

--- Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/CreateAccountActivity.java
+++ Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package ptns.ntu.notes_mobileapp;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -12,11 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.regex.Pattern;
+
 public class CreateAccountActivity extends AppCompatActivity {
 
     EditText emailEditText,passwordEditText,confirmPasswordEditText;
     Button createAccountBtn;
-    ProgressBar processBar;
+    ProgressBar progressBar;
     TextView loginBtnTextView;
 
     @Override
@@ -34,5 +37,34 @@ public class CreateAccountActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.password_edit_text);
         confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
         createAccountBtn = findViewById(R.id.create_account_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+
+        //
+        createAccountBtn.setOnClickListener(v-> createAccount());
+        //
+        loginBtnTextView.setOnClickListener(v->finish());
+
     }
+
+    void createAccount(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+    }
+
+    //phuong thuc tra ve dung hay sai
+    boolean validateData(String email,String password,String confirmPassword){
+        //xác thực dữ liệu được người dùng nhập vào
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        //do dai mat khau
+        if(password.length()<6){
+            passwordEditText.setError("Password length is invalid");
+            return false;
+        }
+    }
+
 }

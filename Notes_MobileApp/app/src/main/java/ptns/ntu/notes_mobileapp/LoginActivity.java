6a9b32d19f17ccd16139f124109032842cfa7e1b
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-06-11 00:19:47 +0700

    update login tai khoan

--- Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/LoginActivity.java
+++ Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/LoginActivity.java
@@ -1,14 +1,32 @@
 package ptns.ntu.notes_mobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    EditText emailEditText,passwordEditText;
+    Button loginBtn;
+    ProgressBar progressBar;
+    TextView createAccountBtnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,83 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        loginBtn = findViewById(R.id.login_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        createAccountBtnTextView = findViewById(R.id.create_account_text_view_btn);
+
+
+        loginBtn.setOnClickListener((v)-> loginUser() );
+        createAccountBtnTextView.setOnClickListener((v)->startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class)));
     }
+
+    void loginUser(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        boolean isValidated = validateData(email,password);
+        if(!isValidated){
+            return;
+        }
+
+        //xac thuc Firebase
+
+        loginAccountInFirebase(email,password);
+
+    }
+
+    //pthuc dang nhap tai khoan trog firebase
+    void loginAccountInFirebase(String email,String password){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        //hien thi vong xoay
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                changeInProgress(false);
+                if(task.isSuccessful()){
+                    //
+                    if(firebaseAuth.getCurrentUser().isEmailVerified()){
+                        //chuyen den main
+
+                    }else{
+
+                    }
+
+                }else{
+                    //login failed
+
+                }
+            }
+        });
+    }
+
+    void changeInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            loginBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            loginBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+
+    //phuong thuc tra ve dung hay sai, xac thuc du lieu
+    boolean validateData(String email,String password){
+        //xác thực dữ liệu được người dùng nhập vào
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        //do dai mat khau
+        if(password.length()<6){
+            passwordEditText.setError("Password length is invalid");
+            return false;
+        }
+        return true;
+    }
+
 }

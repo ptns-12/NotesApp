f07d0351ff3c9f570d8e82e575e1ad8ee65efb84
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-06-10 19:52:18 +0700

    update man hinh khoi dong

---
+++ Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/HelloActivity2.java
@@ -0,0 +1,33 @@
+package ptns.ntu.notes_mobileapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HelloActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_hello2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        // vòng tròn khởi động app
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(HelloActivity2.this,MainActivity.class));
+            }
+        }, 1000); // delay 1s
+    }
+}

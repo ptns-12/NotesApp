86aa70150efd80c2b5dd9fe983fe4b281dc90ec6
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-06-10 22:30:33 +0700

    update database firebase

--- Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/CreateAccountActivity.java
+++ Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/CreateAccountActivity.java
@@ -1,6 +1,10 @@
 package ptns.ntu.notes_mobileapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText emailEditText,passwordEditText,confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar processBar;
+    TextView loginBtnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,10 @@ public class CreateAccountActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_btn);
     }
 }

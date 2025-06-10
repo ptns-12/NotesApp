21997325d997825e5fd2091de01910928873dc4e
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-06-11 02:28:31 +0700

    Update NoteDetailsActivity.java

--- Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/NoteDetailsActivity.java
+++ Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/NoteDetailsActivity.java
@@ -1,6 +1,8 @@
 package ptns.ntu.notes_mobileapp;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class NoteDetailsActivity extends AppCompatActivity {
+
+    EditText titleEditText,contentEditText;
+    ImageButton saveNoteBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,22 @@ public class NoteDetailsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        titleEditText = findViewById(R.id.notes_title_text);
+        contentEditText = findViewById(R.id.notes_content_text);
+        saveNoteBtn = findViewById(R.id.save_note_btn);
+
+        saveNoteBtn.setOnClickListener((v)-> saveNote());
+    }
+    //luu nut ghi chu
+    void saveNote(){
+        String noteTitle = titleEditText.getText().toString();
+        String noteContent = contentEditText.getText().toString();
+        //thong bao phai co tieu de
+        if(noteTitle==null || noteTitle.isEmpty()){
+            titleEditText.setError("Title is required");
+            return;
+        }
+        
     }
 }

0c01ee3bc710628124387fe07ff23f4b70506f57
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-06-10 22:05:32 +0700

    update man hinh dang nhap

--- Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/CreateAccountActivity.java
+++ Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/CreateAccountActivity.java
@@ -1,8 +1,6 @@
 package ptns.ntu.notes_mobileapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HelloActivity2 extends AppCompatActivity {
+public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_hello2);
+        setContentView(R.layout.activity_create_account);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // vòng tròn khởi động app
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(HelloActivity2.this, CreateAccountActivity.class));
-                finish();
-            }
-        }, 1000); // delay 1s
     }
 }

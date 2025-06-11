9ab95dd3ed8ccb2622afcea125cf8919c1f168d5
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-06-11 10:36:46 +0700

    Update NoteAdapter.java

--- Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/NoteAdapter.java
+++ Notes_MobileApp/app/src/main/java/ptns/ntu/notes_mobileapp/NoteAdapter.java
@@ -1,4 +1,21 @@
 package ptns.ntu.notes_mobileapp;
 
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class NoteAdapter {
+    //ke thua tu recyclerview.viewholder
+    class NoteViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titleTextView,contentTextView,timestampTextView;
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.notes_title_text_view);
+            contentTextView = itemView.findViewById(R.id.notes_content_text_view);
+            timestampTextView = itemView.findViewById(R.id.notes_timestamp_text_view);
+        }
+    }
 }
